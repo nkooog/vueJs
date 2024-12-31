7efacceab575eb762cd14172d3b8e5250e92e8d2
Jy_Park <nkooog@naver.com>
2024-12-31 14:50:41 +0900

    login ing...

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -8,8 +8,8 @@
 
               <div class="d-flex justify-content-center py-4">
                 <a href="index.html" class="logo d-flex align-items-center w-auto">
-                  <img src="@/assets/img/logo.png" alt="">
-                  <span class="d-none d-lg-block">NiceAdmin</span>
+                  <img src="@/assets/img/exona_logo_df.svg" alt="">
+                  <span class="d-none d-lg-block"></span>
                 </a>
               </div><!-- End Logo -->
 
@@ -21,28 +21,28 @@
                     <p class="text-center small">Enter your username & password to login</p>
                   </div>
 
-                  <form name="sendForm" class="row g-3 needs-validation" @submit.prevent>
+                  <form name="sendFrom" class="row g-3 needs-validation" novalidate @submit.prevent="validateForm">
 
                     <div class="col-12">
-                      <label for="yourUsername" class="form-label">Tenant</label>
+                      <label for="tenantId" class="form-label">고객사</label>
                       <div class="input-group has-validation">
                         <input type="text" name="tenantId" class="form-control" id="tenantId" required>
-                        <div class="invalid-feedback">Please enter your username.</div>
+                        <div class="invalid-feedback">고객사를 입력해주세요.</div>
                       </div>
                     </div>
 
                     <div class="col-12">
-                      <label for="yourUsername" class="form-label">Username</label>
+                      <label for="usrId" class="form-label">아이디</label>
                       <div class="input-group has-validation">
                         <input type="text" name="usrId" class="form-control" id="usrId" required>
-                        <div class="invalid-feedback">Please enter your username.</div>
+                        <div class="invalid-feedback">아이디를 입력해주세요.</div>
                       </div>
                     </div>
 
                     <div class="col-12">
-                      <label for="yourPassword" class="form-label">Password</label>
+                      <label for="scrtNo" class="form-label">비밀번호</label>
                       <input type="password" name="scrtNo" class="form-control" id="scrtNo" required>
-                      <div class="invalid-feedback">Please enter your password!</div>
+                      <div class="invalid-feedback">비밀번호를 입력해주세요.</div>
                     </div>
 
                     <div class="col-12">
@@ -52,7 +52,7 @@
                       </div>
                     </div>
                     <div class="col-12">
-                      <button class="btn btn-primary w-100" type="submit" @click="login()">Login</button>
+                      <button class="btn btn-primary w-100" type="submit">Login</button>
                     </div>
                     <div class="col-12">
                       <p class="small mb-0">Don't have account? <a href="pages-register.html">Create an account</a></p>
@@ -90,6 +90,10 @@ export default {
     };
   },
   methods : {
+    validateForm() {
+      const form = document.forms['sendFrom'];
+      form.checkValidity() ? this.login() : form.classList.add('was-validated');
+    },
     login() {
 
       const form = document.querySelector('form');
